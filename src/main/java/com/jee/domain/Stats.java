c1a10c1bd98fdb8ada8a2da6c2293a9eb17282f7
gruchanet <grucha.net@gmail.com>
2014-01-14 17:19:17 +0100

    updated

---
+++ src/main/java/com/jee/domain/Stats.java
@@ -0,0 +1,28 @@
+package com.jee.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "stats.all", query = "SELECT st FROM Stats st")
+})
+public class Stats {
+	
+	private Long id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
